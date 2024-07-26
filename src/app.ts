b4098752fabfce1543e21f0b177463efac1468ab
Edwin Ortiz <edarcode@gmail.com>
2024-07-26 01:38:00 -0500

    refactor

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import { Res } from "../types/types";
 import { usersRouter } from "./routes/users";
 
 dotenv.config();
@@ -9,4 +10,5 @@ export const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/", (_req, res: Res) => res.json("asdfasdf"));
 app.use("/api/users", usersRouter);
